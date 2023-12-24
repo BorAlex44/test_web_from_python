0d39c7613bb219dab3c93965c99000a56ee76bea
BorAlex <alexander44@bk.ru>
2023-12-24 18:14:25 +0300

    add seminar 4

---
+++ request.py
@@ -0,0 +1,25 @@
+import logging
+
+import requests
+import yaml
+
+with open('testdata.yaml', encoding='utf-8') as file:
+    data = yaml.safe_load(file)
+
+def get_posts(token):
+    try:
+        get = requests.get(url=data.get('url'), headers={'X-Auth-Token': token}, params={'owner': 'notMe'})
+    except:
+        logging.exception('Exception while get request')
+        get = None
+    if get.status_code == 200:
+        return get.json()
+
+def get_my_posts(token):
+    try:
+        get = requests.get(url=data.get('url'), headers={'X-Auth-Token': token})
+    except:
+        logging.exception('Exception while get request')
+        get = None
+    if get.status_code == 200:
+        return get.json()
